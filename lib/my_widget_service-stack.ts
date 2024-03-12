b8462fb787e2e51e8d8008f6ad4abd5894f04347
Samiullahmalik17 <samiullahmalk17@gmail.com>
2024-03-12 15:18:56 +0500

    Second commit

---
+++ lib/my_widget_service-stack.ts
@@ -0,0 +1,13 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as widget_service from '../lib/widget_service';
+
+export class MyWidgetServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    new widget_service.WidgetService(this, 'Widgets');
+  }
+}
